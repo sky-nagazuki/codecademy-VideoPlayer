85cd935bfeaee6c1ae1e7503d5655de6ea7f3429
sky-nagazuki <hukuda7@gmail.com>
2018-06-29 03:34:10 +0900

    Create Video.js

---
+++ Video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Video extends React.Component {
+  render() {
+    return (
+      <div>
+        <video controls autostart autoplay 
+          src={this.props.src} />
+      </div>
+    );
+  }
+}
